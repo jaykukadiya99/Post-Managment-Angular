6c6f64621fa3d5b296eff982d8cc1ed3e0afd2c6
jaykukadiya99 <jaykukadiya99@gmail.com>
2020-06-24 20:05:54 +0530

    3.9 cors enable

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-with,Content-Type,Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,PATCH");
+    next();
+})
+
 app.use('/api/posts', (req, res, next) => {
     posts = [{
             _id: "dbasjkb",
