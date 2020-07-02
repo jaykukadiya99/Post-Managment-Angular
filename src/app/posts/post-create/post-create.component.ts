e9806a63f1acd33984b1a15a1b8f93049a28e040
jaykukadiya99 <jaykukadiya99@gmail.com>
2020-07-02 13:56:41 +0530

    img preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   private mode ='create';
   public isLoading =true;
   private postId:string;
+  imagePreview:string;
   post:Posts;
   form : FormGroup;
 
@@ -25,6 +26,7 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       title:new FormControl(null,{validators:[Validators.required,Validators.minLength(3)]}),
       content:new FormControl(null,{validators:[Validators.required]}),
+      image:new FormControl(null,{validators:[Validators.required]}),
     });
 
     this.route.paramMap.subscribe((paramMap:ParamMap)=>{
@@ -51,6 +53,16 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
+  onImagePicked(event:Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imagePreview = reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
   onSavePost() {
     if (this.form.invalid) {
       return;
