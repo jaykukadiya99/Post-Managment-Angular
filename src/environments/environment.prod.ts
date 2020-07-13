3ebb9946b5d6eb975b0cbeeeae5252247286856b
jaykukadiya99 <jaykukadiya99@gmail.com>
2020-07-13 16:36:52 +0530

    github.io

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  // apiUrl:"http://nodeapi-env.eba-pkrupxqg.us-east-2.elasticbeanstalk.com/api/"
+  apiUrl:"https://mean-full-app-jk.herokuapp.com/api/"
 };
